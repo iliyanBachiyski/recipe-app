d5d37f739b4787494cc7d427773a6839fd0ff562
iliyanBachiyski <iliyanbachiyski@gmail.com>
2019-05-10 14:59:24 +0300

    Introduced react-router.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import SearchBar from "./components/SearchBar/SearchBar";
 import classes from "./App.module.css";
 import Button from "./components/Button/Button";
 import SearchCard from "./components/SeacrchCard/SearchCard";
+import CardContainer from "./components/CardContainer/CardContainer";
+import { Route } from "react-router-dom";
 
 class App extends Component {
   state = {
@@ -16,13 +18,22 @@ class App extends Component {
     return (
       <div className={classes.App}>
         <div className={classes.Wrapper}>
-          <SearchBar inputConfig={this.state.searchBarConfig.inputConf} />
-          <div className={classes.SearchCardContainer}>
-            <SearchCard />
-            <SearchCard />
-            <SearchCard />
-          </div>
-          <Button label="Search" />
+          <Route
+            path="/"
+            exact
+            render={() => (
+              <React.Fragment>
+                <SearchBar inputConfig={this.state.searchBarConfig.inputConf} />
+                <div className={classes.SearchCardContainer}>
+                  <SearchCard />
+                  <SearchCard />
+                  <SearchCard />
+                </div>
+                <Button label="Search" />
+              </React.Fragment>
+            )}
+          />
+          <Route path="/recipes" render={() => <CardContainer />} />
         </div>
       </div>
     );
